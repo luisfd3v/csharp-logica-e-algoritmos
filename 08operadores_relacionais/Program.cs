3e2546e10cc32257d3f879416d9ed36286c94366
luisfd3v <luisfd3v@gmail.com>
2025-07-14 20:44:29 -0300

    estudos 14/07/2025

---
+++ 08operadores_relacionais/Program.cs
@@ -0,0 +1,21 @@
+﻿// Variáveis
+int numero1 = 5;
+int numero2 = 8;
+
+// Maior
+System.Console.WriteLine(numero1 > numero2);
+
+// Maior ou igual
+System.Console.WriteLine(numero1 >= numero2);
+
+// Menor
+System.Console.WriteLine(numero1 < numero2);
+
+// Menor ou igual
+System.Console.WriteLine(numero1 <= numero2);
+
+// Igual
+System.Console.WriteLine(numero1 == numero2);
+
+// Diferente
+System.Console.WriteLine(numero1 != numero2);
---
+++ 12condicional_operadores_logicos/Program.cs
@@ -0,0 +1,13 @@
+﻿// Variáveis
+double valor = 500;
+string pagamento = "à vista";
+
+// Condicional
+if (valor >= 100 && pagamento == "à vista")
+{
+    Console.WriteLine($"Desconto de 10%! Total a pagar R${valor * 0.9}");
+}
+else
+{
+    Console.WriteLine($"O total da compra é R${valor}");
+}
